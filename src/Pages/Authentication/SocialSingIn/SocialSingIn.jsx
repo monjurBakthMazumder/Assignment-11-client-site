527c595472dfdc2879ff115659816ef5d742726d
md monjur bakth mazumder <md.monjurmbm2001@gmail.com>
2023-11-05 05:35:36 +0600

    done google login

--- src/Pages/Authentication/SocialSingIn/SocialSingIn.jsx
+++ src/Pages/Authentication/SocialSingIn/SocialSingIn.jsx
@@ -1,7 +1,20 @@
+import UseAuth from "../../../Hock/UseAuth";
+
 const SocialSingIn = () => {
+  const {singInWithGoogle} = UseAuth()
+  const handleSocialLogin = media => {
+    media()
+    .then(result=> {
+      console.log(result);
+    })
+    .catch(err=> {
+      console.log(err);
+    })
+  }
   return (
     <div>
       <button
+      onClick={()=> handleSocialLogin(singInWithGoogle)}
         type="button"
         className="w-full py-3 px-4 inline-flex justify-center items-center gap-2 rounded-md border border-blue-500 font-medium bg-white text-blue-600 shadow-sm align-middle hover:bg-blue-500 hover:text-white"
       >
--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -1,17 +1,26 @@
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import PropTypes from "prop-types";
 import { createContext, useState } from "react";
+import auth from "../Firebase/Firebase.config";
 
 export const AuthContext = createContext();
+const googleProvider = new GoogleAuthProvider();
 
 const AuthProvider = ({children}) => {
     const [user, setUser] = useState(null)
     const [isLoading, setIsLoading] = useState(true)
 
+    // sing in with google
+    const singInWithGoogle = () => {
+        setIsLoading(true)
+        return signInWithPopup(auth, googleProvider)
+    }
 
     const authInfo = {
         user,
         setUser,
-        isLoading
+        isLoading,
+        singInWithGoogle
     }
   return <AuthContext.Provider value={authInfo}>
     {children}
