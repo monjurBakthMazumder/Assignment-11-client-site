fc1947b867a849b92c8fc68bd375d76e26d2fe61
md monjur bakth mazumder <md.monjurmbm2001@gmail.com>
2023-11-05 04:54:26 +0600

    add react form in sing in and sing up page

--- src/Pages/Authentication/SingIn/SingIn.jsx
+++ src/Pages/Authentication/SingIn/SingIn.jsx
@@ -2,30 +2,56 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 import { AiFillEye, AiFillEyeInvisible } from "react-icons/ai";
 import SocialSingIn from "../SocialSingIn/SocialSingIn";
+import { useForm } from "react-hook-form";
 const SingIn = () => {
   const [showPassword, setShowPassword] = useState(false);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+  const onSubmit = (data) => {
+    const email = data.email;
+    const password = data.password;
+    console.log(email, password);
+  };
   return (
     <div className="max-w-xl mx-auto my-10 md:my-20 border-2 rounded border-blue-400 p-10">
-      <h1 className="text-3xl md:text-4xl font-bold text-center mb-10">Sing in</h1>
-      <SocialSingIn/>
-      <form>
+      <h1 className="text-3xl md:text-4xl font-bold text-center mb-10">
+        Sing in
+      </h1>
+      <SocialSingIn />
+      <form onSubmit={handleSubmit(onSubmit)}>
         <label htmlFor="email">
           Email
           <input
-            type="email"
-            name="email"
+            {...register("email", {
+              required: "Email is required",
+              pattern: {
+                value: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
+                message: "Invalid email address",
+              },
+            })}
             placeholder="Email"
-            className="py-3 px-4 block w-full border border-blue-200 rounded-md text-sm focus:border-blue-500 focus:ring-blue-500 outline-none mt-2 mb-5"
+            className="py-3 px-4 block w-full border border-blue-200 rounded-md text-sm focus:border-blue-500 focus:ring-blue-500 outline-none mt-2"
           />
         </label>
+        <p className="text-xs text-red-600 mb-5 mt-1">
+          {errors.email?.message}
+        </p>
         <div className="relative">
           <label htmlFor="password">
             Password
             <input
               type={showPassword ? "text" : "password"}
-              name="password"
+              {...register("password", { required: "Password is required" })}
               placeholder="Password"
-              className="py-3 pl-4 pr-12 block w-full border border-blue-200 rounded-md text-sm focus:border-blue-500 focus:ring-blue-500 outline-none mt-2 mb-5"
+              className="py-3 pl-4 pr-12 block w-full border border-blue-200 rounded-md text-sm focus:border-blue-500 focus:ring-blue-500 outline-none mt-2"
             />
           </label>
           <span
@@ -35,6 +61,9 @@ const SingIn = () => {
             {showPassword ? <AiFillEyeInvisible /> : <AiFillEye />}
           </span>
         </div>
+        <p className="text-xs text-red-600 mb-5 mt-1">
+          {errors.password?.message}
+        </p>
         <button
           type="submit"
           className="w-full py-3 px-4 inline-flex justify-center items-center gap-2 rounded-md border border-transparent font-semibold bg-blue-500 text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
@@ -44,7 +73,7 @@ const SingIn = () => {
         <p className="text-xs text-center mt-5">
           New Here? please{" "}
           <Link to={"/sing-up"} className="text-blue-800 underline font-bold">
-            SingUP
+            SingUp
           </Link>
         </p>
       </form>
--- src/Pages/Authentication/SingUp/SingUp.jsx
+++ src/Pages/Authentication/SingUp/SingUp.jsx
@@ -2,48 +2,90 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 import { AiFillEye, AiFillEyeInvisible } from "react-icons/ai";
 import SocialSingIn from "../SocialSingIn/SocialSingIn";
+import { useForm } from "react-hook-form";
 const SingUp = () => {
-    const [showPassword, setShowPassword] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      name: "",
+      photo: "",
+      email: "",
+      password: "",
+    },
+  });
+  const onSubmit = (data) => {
+    const name = data.name;
+    const photo = data.photo;
+    const email = data.email;
+    const password = data.password;
+    console.log(name, photo, email, password);
+  };
     return (
       <div className="max-w-xl mx-auto my-10 md:my-20 border-2 rounded border-blue-400 p-10">
-        <h1 className="text-3xl md:text-4xl font-bold text-center mb-10">Sing Up</h1>
-        <SocialSingIn/>
-        <form>
+        <h1 className="text-3xl md:text-4xl font-bold text-center mb-10">
+          Sing up
+        </h1>
+        <SocialSingIn />
+        <form onSubmit={handleSubmit(onSubmit)}>
           <label htmlFor="name">
             Name
             <input
-              type="text"
-              name="name"
+              {...register("name", {required: "Name is required",})}
               placeholder="Name"
-              className="py-3 px-4 block w-full border border-blue-200 rounded-md text-sm focus:border-blue-500 focus:ring-blue-500 outline-none mt-2 mb-5"
+              className="py-3 px-4 block w-full border border-blue-200 rounded-md text-sm focus:border-blue-500 focus:ring-blue-500 outline-none mt-2"
             />
           </label>
+          <p className="text-xs text-red-600 mb-5 mt-1">
+            {errors.name?.message}
+          </p>
           <label htmlFor="photo">
-            Photo URL
+          Photo URL
             <input
-              type="text"
-              name="photo"
+              {...register("photo", {required: "Photo URL is required",})}
               placeholder="Photo URL"
-              className="py-3 px-4 block w-full border border-blue-200 rounded-md text-sm focus:border-blue-500 focus:ring-blue-500 outline-none mt-2 mb-5"
+              className="py-3 px-4 block w-full border border-blue-200 rounded-md text-sm focus:border-blue-500 focus:ring-blue-500 outline-none mt-2"
             />
           </label>
+          <p className="text-xs text-red-600 mb-5 mt-1">
+            {errors.photo?.message}
+          </p>
           <label htmlFor="email">
             Email
             <input
-              type="email"
-              name="email"
+              {...register("email", {
+                required: "Email is required",
+                pattern: {
+                  value: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
+                  message: "Invalid email address",
+                },
+              })}
               placeholder="Email"
-              className="py-3 px-4 block w-full border border-blue-200 rounded-md text-sm focus:border-blue-500 focus:ring-blue-500 outline-none mt-2 mb-5"
+              className="py-3 px-4 block w-full border border-blue-200 rounded-md text-sm focus:border-blue-500 focus:ring-blue-500 outline-none mt-2"
             />
           </label>
+          <p className="text-xs text-red-600 mb-5 mt-1">
+            {errors.email?.message}
+          </p>
           <div className="relative">
             <label htmlFor="password">
               Password
               <input
                 type={showPassword ? "text" : "password"}
-                name="password"
+                {...register("password", {
+                  required: "Password is required",
+                  pattern: {
+                    value:
+                      /^(?=.*[A-Z])(?=.*\d)(?=.*[!@#\$%^&*()_+{}\[\]:;<>,.?~\\-]).{8,}$/,
+                    message:
+                      "Your password must be one uppercase, one digit, special characters and 8 character longer",
+                  },
+                })}
                 placeholder="Password"
-                className="py-3 pl-4 pr-12 block w-full border border-blue-200 rounded-md text-sm focus:border-blue-500 focus:ring-blue-500 outline-none mt-2 mb-5"
+                className="py-3 pl-4 pr-12 block w-full border border-blue-200 rounded-md text-sm focus:border-blue-500 focus:ring-blue-500 outline-none mt-2"
               />
             </label>
             <span
@@ -53,6 +95,9 @@ const SingUp = () => {
               {showPassword ? <AiFillEyeInvisible /> : <AiFillEye />}
             </span>
           </div>
+          <p className="text-xs text-red-600 mb-5 mt-1">
+            {errors.password?.message}
+          </p>
           <button
             type="submit"
             className="w-full py-3 px-4 inline-flex justify-center items-center gap-2 rounded-md border border-transparent font-semibold bg-blue-500 text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
