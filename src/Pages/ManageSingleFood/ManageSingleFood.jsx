3dbe5cdb114437e41a39b4c80970d24260f1a97d
md monjur bakth mazumder <md.monjurmbm2001@gmail.com>
2023-11-06 06:56:02 +0600

    add disable delete when status === deleverd in my request page

--- src/Pages/ManageSingleFood/ManageSingleFood.jsx
+++ src/Pages/ManageSingleFood/ManageSingleFood.jsx
@@ -1,10 +1,118 @@
-
+import { useEffect, useState } from "react";
+import useAxiosSecure from "../../Hock/axiosSecure";
+import { useTable } from "react-table";
+import { useMemo } from "react";
+import UseAuth from "../../Hock/UseAuth";
+import { useParams } from "react-router-dom";
 const ManageSingleFood = () => {
-    return (
-        <div>
-            ManageSingleFood
-        </div>
-    );
+  const params = useParams();
+  console.log("useparams", params);
+  const [food, setFoods] = useState([]);
+  const axiosSecure = useAxiosSecure();
+  const { user } = UseAuth();
+  console.log("user from manage", user);
+  console.log(food);
+  const data = useMemo(() => food, [food]);
+  const handleUpdate = (id) => {
+    const updateFood = { status : "Delivered"}
+    alert("update");
+    axiosSecure
+      .put(`/request/${id}`, updateFood)
+      .then((response) => {})
+      .catch((error) => {
+        console.error("Error deleting food item: ", error);
+      });
+  };
+
+  const columns = useMemo(
+    () => [
+      {
+        Header: "Requester Name",
+        accessor: "requesterName",
+      },
+      {
+        Header: "Requester Image",
+        accessor: "requesterImg",
+      },
+      {
+        Header: "Requester Email",
+        accessor: "requesterEmail",
+      },
+      {
+        Header: "Request Date",
+        accessor: "requestDate",
+      },
+      {
+        Header: "Status",
+        accessor: "status",
+      },
+      {
+        Header: "Change",
+        accessor: "_id",
+        Cell: ({ row }) => (
+          <div>
+            <p onClick={() => handleUpdate(row.original._id)}>status</p>
+          </div>
+        ),
+      },
+    ],
+    []
+  );
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
+    useTable({ columns, data });
+  useEffect(() => {
+    axiosSecure
+      .get(`/manage-single-foods-request/${params?.id}`)
+      .then((res) => setFoods(res.data));
+  }, [axiosSecure, params?.id]);
+  console.log(food);
+
+  return (
+    <div className="max-w-[100rem] w-full mx-auto  px-4 sm:px-6 lg:px-8 my-10 sm:my-20">
+      <div className="overflow-x-auto">
+        <table
+          {...getTableProps()}
+          className="min-w-full divide-y divide-gray-200"
+        >
+          <thead>
+            {headerGroups.map((headerGroup, i) => (
+              <tr {...headerGroup.getHeaderGroupProps()} key={i}>
+                {headerGroup.headers.map((column, i) => (
+                  <th
+                    {...column.getHeaderProps()}
+                    key={i}
+                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase"
+                  >
+                    {column.render("Header")}
+                  </th>
+                ))}
+              </tr>
+            ))}
+          </thead>
+          <tbody {...getTableBodyProps()} className="divide-y divide-gray-200">
+            {rows.map((row, i) => {
+              prepareRow(row);
+              return (
+                <tr {...row.getRowProps()} key={i}>
+                  {row.cells.map((cell, i) => (
+                    <td
+                      {...cell.getCellProps()}
+                      key={i}
+                      className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800"
+                    >
+                      {cell.column.id === "action"
+                        ? cell.render("Cell")
+                        : cell.render("Cell")}
+                    </td>
+                  ))}
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default ManageSingleFood;
--- src/Pages/MyFoodRequest/MyFoodRequest.jsx
+++ src/Pages/MyFoodRequest/MyFoodRequest.jsx
@@ -3,124 +3,131 @@ import useAxiosSecure from "../../Hock/axiosSecure";
 import { useTable } from "react-table";
 import { useMemo } from "react";
 import UseAuth from "../../Hock/UseAuth";
-import { AiFillDelete, } from 'react-icons/ai';
+import { AiFillDelete } from "react-icons/ai";
 const MyFoodRequest = () => {
-    const [food, setFoods] = useState([]);
-    const axiosSecure = useAxiosSecure();
-    const { user } = UseAuth();
-    console.log("user from manage", user);
-    console.log(food);
-    const data = useMemo(() => food, [food]);
-    const handleDelete = (id) => {
-      alert('delete')
-      axiosSecure
-        .delete(`/request/${id}`)
-        .then((response) => {
-          setFoods((prevFoods) =>
-            prevFoods.filter((food) => food._id !== id)
-          );
-        })
-        .catch((error) => {
-          console.error("Error deleting food item: ", error);
-        });
-    };
-  
-    const columns = useMemo(
-      () => [
-        {
-          Header: "Donator Name",
-          accessor: "donatorName",
-        },
-        {
-          Header: "Pickup location",
-          accessor: "pickupLocation",
-        },
-        {
-          Header: "Expired Date",
-          accessor: "expiredDate",
-        },
-        {
-          Header: "Request Date",
-          accessor: "requestDate",
-        },
-        {
-          Header: "Donation Amount",
-          accessor: "donationMoney",
-        },
-        {
-          Header: "Status",
-          accessor: "status",
-        },
-        {
-          Header: "Action",
-          accessor: "_id",
-          Cell: ({ row }) => (
-            <div>
-              <button onClick={() => handleDelete(row.original._id)}><AiFillDelete className="text-2xl text-blue-600 hover:text-red-600 mx-1"/></button>
-            </div>
-          ),
-        },
-      ],
-      []
-    );
-    const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
-      useTable({ columns, data });
-    useEffect(() => {
-      axiosSecure
-        .get(`/request-foods?email=${user?.email}`)
-        .then((res) => setFoods(res.data));
-    }, [axiosSecure, user?.email]);
-    console.log(food);
-  
-    return (
-        <div className="max-w-[100rem] w-full mx-auto  px-4 sm:px-6 lg:px-8 my-10 sm:my-20">
-          <div className="overflow-x-auto">
-            <table
-              {...getTableProps()}
-              className="min-w-full divide-y divide-gray-200"
-            >
-              <thead>
-                {headerGroups.map((headerGroup,i) => (
-                  <tr {...headerGroup.getHeaderGroupProps()} key={i}>
-                    {headerGroup.headers.map((column,i) => (
-                      <th
-                        {...column.getHeaderProps()}
-                        key={i}
-                        className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase"
-                      >
-                        {column.render("Header")}
-                      </th>
-                    ))}
-                  </tr>
-                ))}
-              </thead>
-              <tbody
-                {...getTableBodyProps()}
-                className="divide-y divide-gray-200"
-              >
-                {rows.map((row,i) => {
-                  prepareRow(row);
-                  return (
-                    <tr {...row.getRowProps()} key={i}>
-                      {row.cells.map((cell,i) => (
-                        <td
-                          {...cell.getCellProps()}
-                          key={i}
-                          className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800"
-                        >
-                          {cell.column.id === "action"
-                            ? cell.render("Cell")
-                            : cell.render("Cell")}
-                        </td>
-                      ))}
-                    </tr>
-                  );
-                })}
-              </tbody>
-            </table>
+  const [food, setFoods] = useState([]);
+  const axiosSecure = useAxiosSecure();
+  const { user } = UseAuth();
+  console.log("user from manage", user);
+  console.log(food);
+  const data = useMemo(() => food, [food]);
+  const handleDelete = (id) => {
+    alert("delete");
+    axiosSecure
+      .delete(`/request/${id}`)
+      .then((response) => {
+        setFoods((prevFoods) => prevFoods.filter((food) => food._id !== id));
+      })
+      .catch((error) => {
+        console.error("Error deleting food item: ", error);
+      });
+  };
+
+  const columns = useMemo(
+    () => [
+      {
+        Header: "Donator Name",
+        accessor: "donatorName",
+      },
+      {
+        Header: "Pickup location",
+        accessor: "pickupLocation",
+      },
+      {
+        Header: "Expired Date",
+        accessor: "expiredDate",
+      },
+      {
+        Header: "Request Date",
+        accessor: "requestDate",
+      },
+      {
+        Header: "Donation Amount",
+        accessor: "donationMoney",
+      },
+      {
+        Header: "Status",
+        accessor: "status",
+      },
+      {
+        Header: "Action",
+        accessor: "_id",
+        Cell: ({ row }) => (
+          <div>
+            {row.original.status !== "Delivered" ? (
+              <button onClick={() => handleDelete(row.original._id)}>
+                <AiFillDelete className="text-2xl text-blue-600 hover:text-red-600 mx-1" />
+              </button>
+            ) : (
+              <button disabled>
+                <AiFillDelete className="text-2xl text-gray-400 mx-1" />
+              </button>
+            )}
           </div>
-        </div>
-    );
+
+          // <div>
+          //   <button onClick={() => handleDelete(row.original._id)}><AiFillDelete className="text-2xl text-blue-600 hover:text-red-600 mx-1"/></button>
+          // </div>
+        ),
+      },
+    ],
+    []
+  );
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
+    useTable({ columns, data });
+  useEffect(() => {
+    axiosSecure
+      .get(`/request-foods?email=${user?.email}`)
+      .then((res) => setFoods(res.data));
+  }, [axiosSecure, user?.email]);
+  console.log(food);
+
+  return (
+    <div className="max-w-[100rem] w-full mx-auto  px-4 sm:px-6 lg:px-8 my-10 sm:my-20">
+      <div className="overflow-x-auto">
+        <table
+          {...getTableProps()}
+          className="min-w-full divide-y divide-gray-200"
+        >
+          <thead>
+            {headerGroups.map((headerGroup, i) => (
+              <tr {...headerGroup.getHeaderGroupProps()} key={i}>
+                {headerGroup.headers.map((column, i) => (
+                  <th
+                    {...column.getHeaderProps()}
+                    key={i}
+                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase"
+                  >
+                    {column.render("Header")}
+                  </th>
+                ))}
+              </tr>
+            ))}
+          </thead>
+          <tbody {...getTableBodyProps()} className="divide-y divide-gray-200">
+            {rows.map((row, i) => {
+              prepareRow(row);
+              return (
+                <tr {...row.getRowProps()} key={i}>
+                  {row.cells.map((cell, i) => (
+                    <td
+                      {...cell.getCellProps()}
+                      key={i}
+                      className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800"
+                    >
+                      {cell.column.id === "action"
+                        ? cell.render("Cell")
+                        : cell.render("Cell")}
+                    </td>
+                  ))}
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default MyFoodRequest;
